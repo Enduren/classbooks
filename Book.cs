d1572e1915976dd9f95a3402002d739714922d51
Enduren <dtennison79@gmail.com>
2021-11-07 13:52:00 -0600

    intial commit

---
+++ Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassprC
+{
+    class Book
+    {
+        //describe a series of book attributes
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassprC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a object called book and giving it a title ,author , and pages
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+            book2.title = "Lord of the Rings";
+            book2.author = "Tolkein";
+            book2.pages = 750;
+
+            Console.WriteLine(book1.pages);
+            Console.ReadLine();
+
+        }
+    }
+}
